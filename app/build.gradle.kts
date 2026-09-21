3f548709a9d9d96496d5bda45b65fe5a129a72e7
actuallyayon <actuallyayon@gmail.com>
2026-09-21 18:07:53 +0600

    build(deps): add AndroidX WorkManager dependency and configure version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     
